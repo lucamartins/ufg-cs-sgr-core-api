e3cb0341ac97c6cb2c25657b6d6c4bc023372e6f
arthu <arthurfpeixoto@gmail.com>
2024-07-10 20:32:51 -0300

    feature: methods for user

--- src/main/java/sgr/com/sgrcoreapi/service/user/UserService.java
+++ src/main/java/sgr/com/sgrcoreapi/service/user/UserService.java
@@ -1,10 +1,17 @@
 package sgr.com.sgrcoreapi.service.user;
 
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import sgr.com.sgrcoreapi.domain.user.User;
 import sgr.com.sgrcoreapi.domain.user.UserRepository;
+import sgr.com.sgrcoreapi.domain.user.UserRoleEnum;
 import sgr.com.sgrcoreapi.service.user.dto.AddUserRequest;
+import sgr.com.sgrcoreapi.service.user.dto.UserUpdateRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +23,43 @@ public class UserService {
         User newUser = new User(addUserRequest);
 
         userRepository.save(newUser);
+    }
+
+    public Page<User> getActiveUsers(int page, int pageSize, UserRoleEnum userRole) {
+        Pageable paging = PageRequest.of(page, pageSize);
+        if (userRole == null) {
+            return userRepository.findByIsDeletedFalse(paging);
+        } else {
+            return userRepository.findByIsDeletedFalseAndRole(userRole, paging);
+        }
+    }
+
+    public Page<User> getAllUser(int page, int pageSize) {
+        Pageable paging = PageRequest.of(page, pageSize);
+        return userRepository.findAll(paging);
+    }
+
+    public User getUserDetails(UUID userId) {
+        return userRepository.findById(userId).orElseThrow();
+    }
+
+    public void updateUser(UUID userId, UserUpdateRequest userUpdateRequest) {
+        User user = userRepository
+                .findByIdAndIsDeletedFalse(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+
+        user.updateUserInfo(userUpdateRequest);
+        userRepository.save(user);
+    }
+
+    public void deleteUser(UUID userId) {
+
+        // TODO: Verify if Waiter have table service in progress
+        User user = userRepository
+                .findByIdAndIsDeletedFalse(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+
+        user.setDeleted(true);
+        userRepository.save(user);
     }
 }
