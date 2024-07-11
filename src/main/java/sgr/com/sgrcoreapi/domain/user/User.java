b83efe5ca6de822dd0a4b2ac770e2f81745a8201
mentejoao <skhulzxd@gmail.com>
2024-07-11 10:55:24 -0300

    fix(user): modify package

--- src/main/java/sgr/com/sgrcoreapi/domain/user/User.java
+++ src/main/java/sgr/com/sgrcoreapi/domain/user/User.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import sgr.com.sgrcoreapi.domain.tableService.TableService;
+import sgr.com.sgrcoreapi.domain.tableservice.TableService;
 import sgr.com.sgrcoreapi.service.user.dto.AddUserRequest;
 
 import java.time.LocalDate;
